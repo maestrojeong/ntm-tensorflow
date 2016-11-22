818c35f947a41372daf9dcbe2443dbe4f93ac28f
Junhong Xu <jxu7@bsu.edu>
2016-11-21 21:24:04 -0500

    finished content based addressing.

--- model/memory.py
+++ model/memory.py
@@ -1,9 +1,9 @@
+from ntm_ops import *
 import tensorflow as tf
-import numpy as np
 
 
 class Memory(object):
-    def __init__(self, mem_size=128, mem_dim=20, batch_size=64):
+    def __init__(self, mem_size=128, mem_dim=20, batch_size=1):
         """
         The dimension of the external memory
 
@@ -18,7 +18,7 @@ class Memory(object):
         """
         self.mem_size = mem_size
         self.mem_dim = mem_dim
-        with tf.variable_scope("external_memory"):
+        with tf.name_scope("external_memory"):
             self.memory = tf.fill((mem_size, mem_dim), 1e-6, name="memory")
 
             # initialize read and write weighting with small values. multiple heads may be added later
@@ -28,43 +28,84 @@ class Memory(object):
             # initialize read vector
             self.read_vector = tf.fill((batch_size, mem_dim), value=1e-6, name="read_vector")
 
-    def update_weighting(self, k, b, g, s, y):
+    def update_weighting(self, key_vector, key_strength, interpolation_factor, shift_weighting, sharpen_factor, is_read):
         """
         Parameters:
         -----------
-        k:
-        b:
-        g:
-        s:
-        y:
+        key_vector: Tensor (mem_size)
+            Key vector emitted by the controller, same shape of memory dimension.
+        key_strength: Tensor (scalar)
+            Key strength emitted by the controller, a scalar.
+        interpolation_factor:
+        shift_weighting:
+        sharpen_factor:
+        is_read: bool
         :return:
         """
         # 3.3.1
-        content_based_weighting = self.content_addressing(k, b)
+        content_based_weighting = self.content_addressing(key_vector, key_strength)
 
         # 3.3.2
-        w = self.location_addressing(content_based_weighting, g, s, y)
+        w = self.location_addressing(content_based_weighting, interpolation_factor, shift_weighting,
+                                     sharpen_factor, is_read)
 
         return w
 
-    def content_addressing(self, k, b):
+    def content_addressing(self, key_vector, key_strength):
         """
         content based addressing, # 3.3.1
 
         Parameters:
         -----------
-        k: Tensor (batch_size, mem_dim)
+        key_vector: Tensor (batch_size, mem_dim)
             Key vector emitted by the controller, same shape of memory dimension.
-        b: Tensor (batch_size, scalar)
+        key_strength: Tensor (batch_size, 1)
             Key strength emitted by the controller, a scalar.
 
         Returns: Tensor (batch_size, mem_size)
             The weighting of content based addressing.
         """
+        with tf.name_scope("content_based_addressing"):
+            # 1. compare similarity and apply key strength
+            similarity = cosine_similarity(key_vector, self.memory) * key_strength
 
+            # 2. normalized weighting
+            w = tf.nn.softmax(similarity, name="content_weighting")
+        return w
 
-    def location_addressing(self, content_based_weighting, g, s, y):
-        pass
+    def location_addressing(self, content_based_weighting, interpolation_factor, shift_weighting,
+                            sharpen_factor, is_read):
+        """
+        location based addressing, # 3.3.2
+
+        Parameters:
+        -----------
+        content_based_weighting: Tensor (batch_size, mem_size)
+            the weighting after content based addressing
+        interpolation_factor: Tensor (batch_size, 1)
+            the scalar used to blend previous weighting and current weighting
+        shift_weighting: Tensor (batch_size, allow_shift_dim)
+            defines allowed integer shift using circular convolution
+        sharpen_factor: Tensor (batch_size, 1)
+            used to sharpen the weighting, let it mainly focus on one location
+        is_read: bool
+            indicate which tensor to return
+
+        Returns: 2 Tensors
+            read_weighting, write_weighting
+        """
+        weighting = self.read_weighting if is_read else self.write_weighting
+        with tf.name_scope("location_based_addressing"):
+            # 1. interpolation
+            new_weighting = interpolation_factor * content_based_weighting + (tf.ones_like(weighting) -
+                                                                              interpolation_factor) * weighting
+
+            # 2. shifting
+            new_weighting = circular_convolution(new_weighting, shift_weighting)
+
+            # 3. sharpening
+
+        return new_weighting
 
     def read(self, read_weighting):
         pass
--- model/ntm_ops.py
+++ model/ntm_ops.py
@@ -1,13 +1,43 @@
 import tensorflow as tf
 
 
-def cosine_similarity(x1, x2):
+def cosine_similarity(key_vector, memory, eps=1e-8):
     """
     The cosine similarity between x1 and x2
 
     Parameters:
     -----------
-    x1: Tensor (any dimension)
-    x2: Tensor ()
-    :return:
+    key_vector: Tensor (Rank of 2)
+        the key vector to compare to the memory
+    memory: Tensor (The last dimension is the same as x1)
+        memory to be compared to
+    eps: float32
+        small float value to prevent divide by 0.
+
+    Returns: Tensor (The first dimension is the same as x1 first dimension,
+                     the second dimension is the same as x2 first dimension)
+    """
+    with tf.name_scope("cos_similarity"):
+        # the numerator of cosine similarity
+        numerator = tf.matmul(key_vector, memory, transpose_b=True)
+
+        # the length of x1 tensor, shape(batch_size)
+        x1_len = tf.reshape(tf.sqrt(tf.reduce_sum(tf.square(key_vector), reduction_indices=1)), shape=(-1, 1))
+
+        # the length of x2 tensor, shape(mem_size)
+        x2_len = tf.reshape(tf.sqrt(tf.reduce_sum(tf.square(memory), reduction_indices=1)), shape=(1, -1))
+
+        denominator = tf.matmul(x1_len, x2_len)
+        return numerator / (denominator + eps)
+
+
+def circular_convolution(weighting, shift_weighting):
+    """
+    Parameter:
+    ----------
+    weighting: Tensor (batch_size, mem_size)
+        the weighting for shifting
+    shift_weighting: Tensor (batch_size, allowed_int_shift)
+
+    Returns
     """
