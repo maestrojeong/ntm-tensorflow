354862c6e5421820a96d035b85229846c1bc99d6
Junhong Xu <jxu7@bsu.edu>
2016-11-24 22:49:06 -0500

    created a NTMCell class

--- model/controller.py
+++ model/controller.py
@@ -1,9 +1,44 @@
 import tensorflow as tf
+import collections
 
 
-class Controller(object):
-    def __init__(self, hidden_dim):
-        self.hidden_dim = hidden_dim
+_NTMStateTuple = collections.namedtuple("NTMStateTuple", ("h", "c", "r"))
 
-    def step(self, x, read_vector, prev_state):
-        pass
+
+class NTMCell(tf.nn.rnn_cell.RNNCell):
+    def __init__(self, unit_num, seq_len, read_len, stacked_layer=1):
+        """
+
+        unit_num:
+        seq_len:
+        read_len:
+        stacked_layer:
+        """
+        self.unit_num = unit_num
+        self.seq_len = seq_len
+        self.stacked_layer = stacked_layer
+        self.read_len = read_len
+
+    def __call__(self, inputs, state, scope=None):
+        """
+        Run NTM cell given the inputs and state
+
+        Parameters:
+        -----------
+        inputs: Tensor (batch_size, input_size)
+            inputs of one time step
+        state: A tuple of three tensors (hidden_state, cell_state, read_vector)
+            state of current time step
+        scope: name of this NTM cell
+
+        Returns:
+            A pair containing: (output(hidden_state), state(hidden_state, cell_state, read_vector))
+        """
+
+    @property
+    def state_size(self):
+        return _NTMStateTuple(self.unit_num, self.unit_num, self.read_len)
+
+    @property
+    def output_size(self):
+        return self.unit_num
